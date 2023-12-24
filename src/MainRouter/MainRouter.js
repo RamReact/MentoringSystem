0089a7e3abc074a415a57b3c8f5e5e09d79d3cd9
Ram <reactram312002@gmail.com>
2023-12-24 15:47:21 +0530

    n

--- src/MainRouter/MainRouter.js
+++ src/MainRouter/MainRouter.js
@@ -10,7 +10,7 @@ export default function MainRouter() {
         <Route />
         {/* Mashers routessssssssssssssssssssssss  */}
         <Route
-          path='/mentor'
+          path='/'
           element={
            
               <Mentor/>
